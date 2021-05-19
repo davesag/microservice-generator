e1dc1fc3852d23b52f71bf364244691276885f95
Dave Sag <davesag@gmail.com>
2021-05-19 13:17:09 +1000

    n/a upgraded dependencies and re-prettiered

--- src/wrap.js
+++ src/wrap.js
@@ -9,30 +9,32 @@ const copy = require('./loadPlugin/copy')
 /* istanbul ignore next */
 const returnUndefined = () => undefined
 
-const wrap = template => async ({ name, description, version, ..._swagger }, swaggerFilePath) => {
-  const buildFolder = path.join(process.cwd(), 'build')
-  const rimraf = promisify(rimrafCb)
+const wrap =
+  template =>
+  async ({ name, description, version, ..._swagger }, swaggerFilePath) => {
+    const buildFolder = path.join(process.cwd(), 'build')
+    const rimraf = promisify(rimrafCb)
 
-  await rimraf(buildFolder)
-  console.log('cleared build folder', buildFolder)
+    await rimraf(buildFolder)
+    console.log('cleared build folder', buildFolder)
 
-  const { repository, next = returnUndefined } = await template(
-    { name, description, version, ..._swagger },
-    swaggerFilePath
-  )
+    const { repository, next = returnUndefined } = await template(
+      { name, description, version, ..._swagger },
+      swaggerFilePath
+    )
 
-  if (!repository) {
-    console.log('No repository found for', name)
-    return
+    if (!repository) {
+      console.log('No repository found for', name)
+      return
+    }
+
+    await loadPlugin(repository, buildFolder)
+    await clean(buildFolder)
+    await updatePackage(buildFolder, { repository, name, description, version })
+    await copy(swaggerFilePath, path.join(buildFolder, 'api.yml'))
+    // generate paths
+    // generate definitions
+    return next()
   }
-
-  await loadPlugin(repository, buildFolder)
-  await clean(buildFolder)
-  await updatePackage(buildFolder, { repository, name, description, version })
-  await copy(swaggerFilePath, path.join(buildFolder, 'api.yml'))
-  // generate paths
-  // generate definitions
-  return next()
-}
 
 module.exports = wrap
